411a041ff283bc25d0406350468a6b6062caf2b0
ioanarebrisorean <ioana.rebrisorean@yahoo.com>
2020-02-19 10:20:54 +0200

    5

--- src/com/Exercices/Whatever/Hello.java
+++ src/com/Exercices/Whatever/Hello.java
@@ -4,15 +4,8 @@ import java.util.Scanner;
 
 public class Hello {
 
-    public float a;
-    public float b;
 
-    public void readNumbers(){
-        Scanner division = new Scanner(System.in);
-        System.out.println("a= ");
-        division.nextFloat();
-        System.out.println("b= ");
-    }
+
 
     public void hello(){
         System.out.println("Hello");
@@ -20,7 +13,5 @@ public class Hello {
         System.out.println("5 + 7 = " + (5 + 7));
     }
 
-    public float division(){
-        return  (a / b);
-    }
+
 }
--- src/com/Exercices/Whatever/Main.java
+++ src/com/Exercices/Whatever/Main.java
@@ -3,11 +3,31 @@ package com.Exercices.Whatever;
 public class Main {
 
     public static void main(String[] args) {
+//Assignment1: a Printati "Hello" in consola, iar pe rand nou, printati numele vostru.
+        // b:  Printati rezultatul sumei a doua numere(orice numere)
 
-        Hello hello = new Hello();
-        hello.readNumbers();
+//        Hello hello = new Hello();
 //        hello.hello();
-        hello.division();
+
+//Assignment2: Printati rezultatul impartirii a doua numere. Atentie, daca numarul nu este intreg, sa afiseze si valorile dupa virgula.
+//        System.out.println(6f/4f);
+
+//Assignment3: Printati rezultatul urmatoarelor operatiuni:
+//        int a = -5 + 8 * 6;
+//        int b = ( 55 + 9 ) % 9;
+//        float c = 20 + -3 * 5f / 8f;
+//        float d = 5 + 15f / 3f * 2 - 8 % 3;
+//        System.out.println("a= " + a);
+//        System.out.println("b = " + b);
+//        System.out.println("c = " + c);
+//        System.out.println("d = " + d);
+
+
+
+
+
+
+
 
 
 
