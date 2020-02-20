70030cd3e5746a319527f8657d75750491538814
ioanarebrisorean <ioana.rebrisorean@yahoo.com>
2020-02-20 09:30:00 +0200

    5

--- src/com/Exercices/Whatever/Main.java
+++ src/com/Exercices/Whatever/Main.java
@@ -1,11 +1,18 @@
 package com.Exercices.Whatever;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.sql.SQLOutput;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
+//    public static void appendElToSet(HashSet<Integer> set, int elToAdd){
+//        set.add(elToAdd);
+//    }
+
     public static void main(String[] args) {
+
 
 
         //Assignment1: a Printati "Hello" in consola, iar pe rand nou, printati numele vostru.
@@ -17,7 +24,7 @@ public class Main {
         //Assignment2: Printati rezultatul impartirii a doua numere. Atentie, daca numarul nu este intreg, sa afiseze si valorile dupa virgula.
 //        System.out.println(6f/4f);
 //
-//  Assignment3: Printati rezultatul urmatoarelor operatiuni:
+        //Assignment3: Printati rezultatul urmatoarelor operatiuni:
 //        int a = -5 + 8 * 6;
 //        int b = ( 55 + 9 ) % 9;
 //        float c = 20 + -3 * 5f / 8f;
@@ -64,7 +71,73 @@ public class Main {
 //        System.out.println(rest(10,7));
 
         //Assignment8: Scrieti o metoda java, care sa primeasca ca si parametru un numar, care sa reprezinte temperatura in Fahrenheit, si sa returneze temperatura in Celsius. Formula de calcul este: C = 5/9 * (F -32
-        System.out.println("Temp Celsius = " + celsius());
+//        System.out.println("Temp Celsius = " + celsius());
+
+        //Assignment9: Write a Java program to append the specified element to the end of a hash set
+        System.out.println("Exercice 1 from HashMap");
+        HashSet<Integer> set1 = new HashSet<Integer>();
+        for ( int i=0; i<=1000; i++){
+            set1.add(i);
+        }
+
+//        appendElToSet(set1, 1506);
+
+
+        //Assignment10: Write a Java program to iterate through all elements in a hash list
+           HashSet<String> hs = new HashSet<String>();
+        hs.add("Welcome");
+        hs.add("to");
+        hs.add("my");
+        hs.add("HashMap");
+        hs.add("and");
+        hs.add("another");
+        hs.add("words");
+
+        System.out.println("My HashMap is " + hs);
+
+        Iterator value = hs.iterator();
+
+        System.out.println("My iterator values are: ");
+
+        while (value.hasNext()){
+            System.out.println(value.next());
+        }
+
+        //Assignment11: Write a Java program to get the number of elements in a hash set
+            System.out.println("Size of the Hash Set is: " + hs.size());
+
+        //Assignment12: Write a Java program to empty an hash set
+//        hs.removeAll(hs);
+//        System.out.println("My Hash Set after removing is " + hs);
+
+        //Assignment13: Write a Java program to test a hash set is empty or not
+        System.out.println("Checking if it's empty " + hs.isEmpty());
+
+        //Assignment14: Write a Java program to clone a hash set to another hash se
+        HashSet <String> newHs = new HashSet <String>();
+        newHs = (HashSet)hs.clone();
+        System.out.println("Colned Hash Set: " + newHs);
+
+//        //Assignment15: Write a Java program to compare two hash set
+//        HashSet <String> hs2 = new HashSet<String>();
+//        hs2.add("Welcome");
+//        hs2.add("here");
+//        HashSet<String>result= new HashSet<String>();
+//        for (String element : hs) {
+//            System.out.println(hs2.contains(element) ? "Yes" : "No");
+//        }
+//    }
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
@@ -130,6 +203,8 @@ public class Main {
         float celsius = 5f / 9f * (fahrenheit - 32);
         return celsius;
     }
+
+
 
 
 
